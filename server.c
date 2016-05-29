2a75176f2afe7e587af2162536313213397aa98e
Yuhangil <rlfdldjaak22@daum.net>
2016-05-29 20:28:28 +0900

    log 만들기. + 들여쓰기

--- server.c
+++ server.c
@@ -7,13 +7,16 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+void makelog(char *query);
+
 int main(int argc, char *argv[]){
     int client, server, n;
     struct sockaddr_in c_addr, s_addr;
     socklen_t len;
     char recv_query[30];
 
-    if(argc < 2){
+    if(argc < 2)
+    {
       puts("Usage: ./server -p port");
       exit(1);
     }
@@ -24,29 +27,43 @@ int main(int argc, char *argv[]){
     s_addr.sin_family = AF_INET;
     s_addr.sin_port = htons(atoi(argv[2]));
 
-    if(bind(server, (struct sockaddr*)&s_addr, sizeof(s_addr)) == -1){
+    if(bind(server, (struct sockaddr*)&s_addr, sizeof(s_addr)) == -1)
+    {
         puts("bind() error");
         exit(1);
     }
 
-    if(listen(server, 5) == -1){
+    if(listen(server, 5) == -1)
+    {
         puts("listen() error");
         exit(1);
     }
 
     len = sizeof(c_addr);
     client = accept(server, (struct sockaddr*)&c_addr, &len);
-    if(client == -1){
+    if(client == -1)
+    {
       puts("Client connection failed");
       exit(1);
     }
 
-    while(1){
+    while(1)
+    {
         read(client, recv_query, 30);
         n = strlen(recv_query);
+        makelog(recv_query);
         write(client, recv_query, n+1);
         puts(recv_query);
     }
     close(client);
     return 0;
 }
+
+void makelog(char *query)
+{
+    FILE *fp;
+    fp = fopen("log", "a+");
+    fprintf(fp, "%s\n", query);
+    fclose(fp);
+}
+
